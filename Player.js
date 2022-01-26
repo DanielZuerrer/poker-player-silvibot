dbc84d07322876d4e070eb76859182334e51edd0
Jonas Huber <jonas.huber@digitecgalaxus.ch>
2022-01-26 10:10:56 +0100

    bet if A

--- Player.js
+++ Player.js
@@ -4,7 +4,26 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
-    bet(10000);
+
+    const playerCards = gameState.players[in_action].hole_cards
+
+    const rankA = playerCards.filter((card) => card.rank === "A")
+    const rankK = playerCards.filter((card) => card.rank === "K")
+    const rankQ = playerCards.filter((card) => card.rank === "Q")
+    const rankJ = playerCards.filter((card) => card.rank === "J")
+    const rank10 = playerCards.filter((card) => card.rank === "10")
+    const rank9 = playerCards.filter((card) => card.rank === "9")
+    const rank8 = playerCards.filter((card) => card.rank === "8")
+    const rank7 = playerCards.filter((card) => card.rank === "7")
+    const rank6 = playerCards.filter((card) => card.rank === "6")
+    const rank5 = playerCards.filter((card) => card.rank === "5")
+    const rank4 = playerCards.filter((card) => card.rank === "4")
+    const rank3 = playerCards.filter((card) => card.rank === "3")
+    const rank2 = playerCards.filter((card) => card.rank === "2")
+
+    if (rankA.length) {
+      bet(10000)
+    }
   }
 
   static showdown(gameState) {
